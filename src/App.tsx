ff2f6a825f55540e8b510bc9fe2d02819003c804
강성훈 <kshevl123@gmail.com>
2025-12-03 23:04:33 +0900

    feat: Route 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,35 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import WebSiteLayout from "@/layout"
-import TouristAttraction from "@/page/TouristAttraction"
-import Festival from "@/page/Festival"
-import Accommodation from "@/page/Accommodation"
-import Tracking from "@/page/Tracking"
-import Gallery from "@/page/Gallery"
-import StartPage from "@/page/Start"
-import TouristAttractionDetail from "@/page/TouristAttractionDetail"
-import FestivalDetail from "@/page/FestivalDetail"
-import AccommodatioDetail from "@/page/AccommodationDetail"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TouristAttraction from "@/page/TouristAttraction";
+import Festival from "@/page/Festival";
+import Accommodation from "@/page/Accommodation";
+import Tracking from "@/page/Tracking";
+import Gallery from "@/page/Gallery";
+import StartPage from "@/page/Start";
+import TouristAttractionDetail from "@/page/TouristAttractionDetail";
+import FestivalDetail from "@/page/FestivalDetail";
+import AccommodationDetail from "@/page/AccommodationDetail";
+import MainPage from "@/page/Main";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<StartPage />}></Route>
-        <Route element={<WebSiteLayout />}>
-          <Route path="TouristAttraction" element={<TouristAttraction />} />
-          <Route path="Festival" element={<Festival />} />
-          <Route path="Accommodation" element={<Accommodation />} />
-          <Route path="Tracking" element={<Tracking />} />
-          <Route path="Gallery" element={<Gallery />} />
-          <Route path="TouristAttraction/:id" element={<TouristAttractionDetail />} />
-          <Route path="Festival/:id" element={<FestivalDetail />} />
-          <Route path="Accommedation/:id" element={<AccommodatioDetail />} />
-        </Route>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/touristAttraction" element={<TouristAttraction />} />
+        <Route path="/festival" element={<Festival />} />
+        <Route path="/accommodation" element={<Accommodation />} />
+        <Route path="/tracking" element={<Tracking />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route
+          path="/touristAttraction/:id"
+          element={<TouristAttractionDetail />}
+        />
+        <Route path="/festival/:id" element={<FestivalDetail />} />
+        <Route path="/accommodation/:id" element={<AccommodationDetail />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
