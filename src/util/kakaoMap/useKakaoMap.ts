5e61a5f580ec6e25ec87b2d114845418d02ed67e
강성훈 <kshevl123@gmail.com>
2025-12-04 22:36:43 +0900

    refactor: 카카오지도 로직 수정

--- src/util/kakaoMap/useKakaoMap.ts
+++ src/util/kakaoMap/useKakaoMap.ts
@@ -9,32 +9,27 @@ function useKakaoMap({ lat, lng }: MapProps) {
   const [map, setMap] = useState<kakao.maps.Map | null>(null);
 
   useEffect(() => {
-    const container = document.getElementById('map');
-    if (!container || !lat || !lng) return;
+  if (!window.kakao || !window.kakao.maps) return;
+  
+  const container = document.getElementById('map');
+  if (!container || !lat || !lng) return;
 
-    const latitude = Number(lat);
-    const longitude = Number(lng);
+  const latitude = Number(lat);
+  const longitude = Number(lng);
 
-    const position = new kakao.maps.LatLng(latitude, longitude);
-    
-    const options = {
-      center: position,
-      level: 5,
-    };
+  const position = new window.kakao.maps.LatLng(latitude, longitude);
+  
+  const options = {
+    center: position,
+    level: 5,
+  };
 
-    const newMap = new kakao.maps.Map(container, options);
+  const newMap = new window.kakao.maps.Map(container, options);
+  const marker = new window.kakao.maps.Marker({ position });
+  marker.setMap(newMap);
 
-    const marker = new kakao.maps.Marker({
-      position: position,
-    });
-    marker.setMap(newMap);
-
-
-    return () => {
-      setMap(null);
-    };
-
-  }, [lat, lng]);
+  setMap(newMap);
+}, [lat, lng]);
 
   return map;
 }
