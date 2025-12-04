3c395139fd0e29ce250edaead7b1abd6ed43cba5
강성훈 <kshevl123@gmail.com>
2025-12-04 19:57:43 +0900

    style: import 수정

--- src/page/Accommodation/index.tsx
+++ src/page/Accommodation/index.tsx
@@ -3,8 +3,8 @@ import { twMerge } from 'tailwind-merge';
 import MainLayout from "@/layout/MainLayout";
 import { SIGNGU } from "@/constant/signgu";
 import { PAGE_SIZE } from "@/constant/pagination";
-import Pagination from "@/common/Components/Pagination";
-import LoadingSpinner from "@/common/Components/LoadingSpinner";
+import Pagination from "@/components/Pagination";
+import LoadingSpinner from "@/components/LoadingSpinner";
 import { useAccommodationListPagination } from "./hooks/useAccommodation";
 import AccommodationList from "./components/AccommodationList";
 
