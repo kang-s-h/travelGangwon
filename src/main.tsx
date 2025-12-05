299024e03920f2a7386bc02578ce9f8fd3ea8bd1
강성훈 <kshevl123@gmail.com>
2025-12-05 11:25:34 +0900

    feat: query staleTime과  gcTime 연장

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,14 @@ import './index.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 60,
+      gcTime: 1000 * 60 * 60 * 2,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
